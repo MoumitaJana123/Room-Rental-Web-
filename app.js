18a4680bd9bec0bf5e0f33155c1a0a7d2b09e2cf
MoumitaJana123 <moumita2003jana@gmail.com>
2026-06-11 12:14:54 +0530

    Refactor 404 handler to use app.use

--- app.js
+++ app.js
@@ -126,7 +126,7 @@ app.get("/", (req, res) => {
 // 404 HANDLER
 // ======================================================
 
-app.all("*", (req, res, next) => {
+app.use((req, res, next) => {
     next(new ExpressError(404, "Page Not Found"));
 });
 
