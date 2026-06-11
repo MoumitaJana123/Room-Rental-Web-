a70e87752d1fd4effdd8987efd6b1bd687cb296d
MoumitaJana123 <moumita2003jana@gmail.com>
2026-06-11 13:51:58 +0530

    Add newline at end of user.js
    Fixed formatting by adding a newline at the end of the file.

--- routes/user.js
+++ routes/user.js
@@ -6,7 +6,6 @@ const passport = require("passport");
 const { saveRedirectUrl } = require("../middleware.js");
 const userController= require("../controllers/users.js")
 
-
 router.route("/signup")
 .get(userController.renderSignupForm)
 .post(wrapAsync(userController.signup));
